7ac93585b179bd0aa296f9623f51cf4320bb071f
buthanoid <portail@buthanoid.eu>
2021-10-01 10:45:56 +0200

    New output keywords: added simpleDateFormat for keywords in ImageOIParam

--- src/main/java/fr/jmmc/oitools/image/ImageOiParam.java
+++ src/main/java/fr/jmmc/oitools/image/ImageOiParam.java
@@ -23,6 +23,7 @@ import fr.jmmc.oitools.fits.FitsHeaderCard;
 import fr.jmmc.oitools.fits.FitsTable;
 import fr.jmmc.oitools.meta.KeywordMeta;
 import fr.jmmc.oitools.meta.Types;
+import java.text.SimpleDateFormat;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Map;
@@ -30,6 +31,8 @@ import java.util.Set;
 
 /** Common behaviour between ImageOIIntputParam & ImageOIOutputParam. */
 public abstract class ImageOiParam extends FitsTable {
+
+    public final static SimpleDateFormat keywordDateFormat = new SimpleDateFormat("yyyy/MM/dd kk'h'mm:ss Z");
 
     /* members */
     /** flag indicating if the default keywords are being defined */
