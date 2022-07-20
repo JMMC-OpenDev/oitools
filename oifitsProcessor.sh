be4701eb4b09b9b2de65ef7363fd894e86ef9fee
bourgesl <bourges.laurent@gmail.com>
2022-07-20 16:52:58 +0200

    fixed oifitsProcessor to use full jar

--- oifitsProcessor.sh
+++ oifitsProcessor.sh
@@ -6,4 +6,5 @@
 #   ./oifitsProcessor.sh list  /home/bourgesl/oidata/2007-06-29.fits
 #
 
-java -cp ./target/oitools-TRUNK.jar fr.jmmc.oitools.OIFitsProcessor $*;
+java -cp ./target/oitools-TRUNK-jar-with-dependencies.jar fr.jmmc.oitools.OIFitsProcessor $*;
+
